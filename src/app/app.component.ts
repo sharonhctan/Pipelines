6eab1a42f45331537775111f29734e36ca2e850f
Sharon Tan <sharon.tan@bannerflow.com>
2022-08-16 14:30:54 +0200

    remove api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // title = 'Tour of Heroes';
-  message = '';
+  title = 'Tour of Heroes';
 
-  constructor(private http: HttpClient) {
-    this.http.get('/api/message')
-      .subscribe((resp: any) => this.message = resp.text);
-  }
 }
