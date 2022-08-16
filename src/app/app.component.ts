06d78116fe9af83d05ca26b53dd6db6e17d22e33
Sharon Tan <sharon.tan@bannerflow.com>
2022-08-16 14:06:40 +0200

    added api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+  // title = 'Tour of Heroes';
+  message = '';
+
+  constructor(private http: HttpClient) {
+    this.http.get('/api/message')
+      .subscribe((resp: any) => this.message = resp.text);
+  }
 }
